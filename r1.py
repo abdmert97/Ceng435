a913728a31880800eb47edbede860339465bf0ee
mert şafak <abdurrahmansfk@hotmail.com>
2019-11-20 23:55:13 +0300

    Create r1.py

---
+++ r1.py
@@ -0,0 +1,28 @@
+import config
+
+from socket import *
+
+#Port of this server.
+localServerPort = 30211
+
+#IP of Destination
+destServerName = '10.10.1.2'
+
+#Port of Destination
+destServerPort = 30212
+
+#Listen to localServerPort.
+localSocket = socket(AF_INET, SOCK_DGRAM)
+localSocket.bind(('10.10.2.2', localServerPort))
+
+#Initialize destSocket and connect to Destination.
+destSocket = socket(AF_INET, SOCK_DGRAM)
+destSocket.connect((destServerName, destServerPort))
+
+while True:
+    (dataFBroker, addrBroker) = localSocket.recvfrom(config.msg_size)
+    #Forward the sentence to Destination
+    destSocket.send(dataFBroker)
+
+localSocket.close()
+destSocket.close()
