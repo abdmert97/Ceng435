6e43a209d1779afb204fe1abbdb1f16eb093d941
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 02:08:33 +0300

    Update r1_d.py

--- r1_d.py
+++ r1_d.py
@@ -34,17 +34,20 @@ for sv in servers: sv.start()
 serverAddressPorts = [("10.10.4.1", 30210), ("10.10.4.1", 30410)]
 
 def client(i): 
-    for x in range(1):
+    for x in range(0,5):
         # Create a UDP socket at client side
         UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        #UDPClientSocket.connect((serverAddressPort))
+        #UDPClientSocket.connect((serverAddressPorts[i]))
         # Send to server using created UDP socket
-        UDPClientSocket.sendto(bytesToSend, serverAddressPort[i])
+        UDPClientSocket.sendto(bytesToSend, serverAddressPorts[i])
+        a = datetime.datetime.now()
 
         msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-        
+        b = datetime.datetime.now()
+        c = b - a
+        print(c.microseconds/1000.0)
         msg = "Message from Server {}".format(msgFromServer[0])
-        print(msg)
+        #print(msg)
 
-#clients = [Thread(target=client, args=(i,)) for i in range(2)]
-#for cl in clients: cl.start()
+clients = [Thread(target=client, args=(i,)) for i in range(2)]
+for cl in clients: cl.start()
