129cea219d414f9d40cc27cab2f06011a6cd3af9
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 02:47:30 +0300

    Create r3_d.py

---
+++ r3_d.py
@@ -0,0 +1,54 @@
+import socket
+import datetime
+from threading import Thread 
+
+localIP     ="10.10.6.1"
+localPorts   = [30300, 30340]
+bufferSize  = 1024
+
+msgFromClient = "Hello UDP Server"
+
+bytesToSend = str.encode(msgFromClient)
+
+
+
+def server(i):  
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPServerSocket.bind((localIP, localPorts[i]))
+    while(True):
+        print("UDP thread"+str(i)+" up and listening")
+        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
+        message = bytesAddressPair[0]
+        address = bytesAddressPair[1]
+        clientMsg = "Message from Client:{}".format(message)
+        clientIP  = "Client IP Address:{}".format(address)
+        print(clientMsg)
+        print(clientIP)
+
+        bytesToSend         = str.encode(str(message).upper())
+        UDPServerSocket.sendto(bytesToSend, address)
+servers = [Thread(target=server, args=(i,)) for i in range(2)]
+for sv in servers: sv.start()
+
+
+serverAddressPorts = [("10.10.5.1", 30230)]
+
+def client(i): 
+    for x in range(0,1):
+        # Create a UDP socket at client side
+        UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        #UDPClientSocket.connect((serverAddressPorts[i]))
+        # Send to server using created UDP socket
+        UDPClientSocket.sendto(bytesToSend, serverAddressPorts[i])
+        a = datetime.datetime.now()
+
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        b = datetime.datetime.now()
+        c = b - a
+        print(c.microseconds/1000.0)
+        msg = "Message from Server {}".format(msgFromServer[0])
+        #print(msg)
+
+clients = [Thread(target=client, args=(i,)) for i in range(1)]
+for cl in clients: cl.start()
