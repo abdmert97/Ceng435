bb55a206846a233408872173fa54212264b0ade7
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 00:09:24 +0300

    Create sendertest2.py

---
+++ sendertest2.py
@@ -0,0 +1,25 @@
+import socket
+from threading import Thread 
+
+msgFromClient = "Hello UDP Server"
+
+bytesToSend = str.encode(msgFromClient)
+
+serverAddressPort = ("10.10.5.1", 30211)
+
+bufferSize = 1024
+def client(): 
+    for i in range(0,5):
+        # Create a UDP socket at client side
+        UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        #UDPClientSocket.connect((serverAddressPort))
+        # Send to server using created UDP socket
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        
+        msg = "Message from Server {}".format(msgFromServer[0])
+        print(msg)
+
+clients = [Thread(target=client, args=()) for i in range(5)]
+for cl in clients: cl.start()
