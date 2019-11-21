6ec4e6b820e887df7a52b1ece1f4e7812bffbcf6
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 00:02:26 +0300

    Update 2waytest.py

--- 2waytest.py
+++ 2waytest.py
@@ -2,20 +2,18 @@ import socket
 from threading import Thread 
 
 localIP     ="10.10.4.1"
-localPort   = 30211
+localPorts   = [30211, 30222]
 bufferSize  = 1024
 
 msgFromClient = "Hello UDP Server"
 
 bytesToSend = str.encode(msgFromClient)
 
-serverAddressPort = ("10.10.4.1", 30211)
-
 
 
 def server(i):  
     UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    UDPServerSocket.bind((localIP, localPort))
+    UDPServerSocket.bind((localIP, localPorts[i]))
     while(True):
         print("UDP thread"+str(i)+" up and listening")
         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
@@ -29,22 +27,24 @@ def server(i):
 
         bytesToSend         = str.encode(str(message).upper())
         UDPServerSocket.sendto(bytesToSend, address)
-servers = [Thread(target=server, args=(i,)) for i in range(5)]
+servers = [Thread(target=server, args=(i,)) for i in range(2)]
 for sv in servers: sv.start()
 
 
-def client(): 
-    for i in range(0,5):
+serverAddressPorts = [("10.10.4.1", 30211), ("10.10.4.1", 30222)]
+
+def client(i): 
+    for x in range(1):
         # Create a UDP socket at client side
         UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         #UDPClientSocket.connect((serverAddressPort))
         # Send to server using created UDP socket
-        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort[i])
 
         msgFromServer = UDPClientSocket.recvfrom(bufferSize)
         
         msg = "Message from Server {}".format(msgFromServer[0])
         print(msg)
 
-clients = [Thread(target=client, args=()) for i in range(5)]
-for cl in clients: cl.start()
+#clients = [Thread(target=client, args=(i,)) for i in range(2)]
+#for cl in clients: cl.start()
