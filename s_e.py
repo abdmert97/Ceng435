66823a46bd7c248f2b691122b29c27838c732a35
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 19:31:40 +0300

    Update s_e.py

--- s_e.py
+++ s_e.py
@@ -5,15 +5,16 @@ r3Address = ("10.10.6.2", 30300)
 
 bufferSize          = 1024
 
-# Create a UDP socket at client side
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+for i in range(1000):
+    # Create a UDP socket at client side
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-a = datetime.datetime.now()
-msgFromClient = str(a)
-bytesToSend         = str.encode(msgFromClient)
-UDPClientSocket.sendto(bytesToSend, r3Address)
+    a = datetime.datetime.now()
+    msgFromClient = str(a)
+    bytesToSend         = str.encode(msgFromClient)
+    UDPClientSocket.sendto(bytesToSend, r3Address)
 
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
- 
-msg = "Message from Server {}".format(msgFromServer[0])
-print(msg)
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+    msg = "Message from Server {}".format(msgFromServer[0])
+    print(msg)
