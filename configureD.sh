13e82e4f69133346c44e12d7a3ddfbe9dfa7a62c
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 20:46:54 +0300

    Update configureD.sh

--- configureD.sh
+++ configureD.sh
@@ -2,4 +2,4 @@ r3=$(getent ahosts "r3" | cut -d " " -f 1 | uniq)
 
 r3_adapter=$(ip route get $r3 | grep -Po '(?<=(dev )).*(?= src| proto)')
 
-sudo tc qdisc add dev $r3_adapter root netem delay $1ms $2ms distribution normal
+sudo tc qdisc add dev $r3_adapter root netem delay 40ms 10ms distribution normal
