c3386d3da6b8dd70f8cf5a6497afba34552f2578
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 23:31:02 +0300

    Update s_f.py

--- s_f.py
+++ s_f.py
@@ -5,27 +5,34 @@ from threading import Thread
 localIP     ="10.10.2.2"
 localPorts   = [30010, 30020, 30030]
 
+# Count of server nodes to be started
+svCount = 0
+
+# Count of messages to be sent or received
 msgCount = 1000
 bufferSize  = 1024
 
 # Init
 
-def server(i):  
+def server(i):   
+    # Initialize the server socket and bind to localip&localport
     UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     UDPServerSocket.bind((localIP, localPorts[i]))
-    for x in range(1000):
-        #print("UDP thread"+str(i)+" up and listening")
+    # Messaging procedure of server
+    for x in range(msgCount):
+        # Wait until the message is received from client
         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-
+        # Get data from received package
         message = bytesAddressPair[0]
         address = bytesAddressPair[1]
-        clientMsg = "Message from Client:{}".format(message)
-        clientIP  = "Client IP Address:{}".format(address)
-        #print(clientMsg)
-        #print(clientIP)
-
-        bytesToSend = str.encode(str(message).upper())
+        # Send the response(upper case version of received message)
+        bytesToSend         = str.encode(str(message).upper())
         UDPServerSocket.sendto(bytesToSend, address)
+    # Close the server socket
     UDPServerSocket.close()
-servers = [Thread(target=server, args=(i,)) for i in range(3)]
+    
+    
+# Run the server threads
+servers = [Thread(target=server, args=(i,)) for i in range(svCount)]
 for sv in servers: sv.start()
+for sv in servers: sv.join()
