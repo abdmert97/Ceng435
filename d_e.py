8a705ce8fe3720fd6990e4f55516d2864cbe1f35
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 18:31:21 +0300

    Update d_e.py

--- d_e.py
+++ d_e.py
@@ -1,26 +1,20 @@
 import socket
 
-localIP     ="10.10.4.1"
-localPort   = 30211
+localIP     ="10.10.5.2"
+localPort   = 30430
 bufferSize  = 1024
 
-msgFromServer       = "Hello UDP Client"
-bytesToSend         = str.encode(msgFromServer)
+
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
-  
-print("UDP server up and listening")
 
+bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
 
-while(True):
+message = bytesAddressPair[0]
+address = bytesAddressPair[1]
+clientMsg = "Message from Client:{}".format(message)
+clientIP  = "Client IP Address:{}".format(address)
+print(clientMsg)
+print(clientIP)
 
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
-    print(clientMsg)
-    print(clientIP)
-
-    UDPServerSocket.sendto(bytesToSend, address)
+UDPServerSocket.sendto(message, address)
