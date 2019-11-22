95fdd6f1baa851ab6c55d442398705892b9faa40
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 19:22:00 +0300

    Update d_e.py

--- d_e.py
+++ d_e.py
@@ -15,7 +15,7 @@ address = bytesAddressPair[1]
 
 raw_message = "{}".format(message)
 sendtime = datetime.datetime.strptime(raw_message, "%Y-%m-%d %H:%M:%S.%f")
-print("formatted: " + str((sendtime.microsecond/1000)/1000.0))
+print("formatted: " + str(sendtime.microsecond-datetime.datetime.now().microsecond/1000.0))
 clientMsg = "Message from Client:{}".format(message)
 clientIP  = "Client IP Address:{}".format(address)
 print(clientMsg)
