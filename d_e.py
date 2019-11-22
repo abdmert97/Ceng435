f6e608566455a517d2b2652d6e7270e897a7886f
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 20:28:02 +0300

    Update d_e.py

--- d_e.py
+++ d_e.py
@@ -4,6 +4,7 @@ localIP     ="10.10.5.2"
 localPort   = 30430
 bufferSize  = 1024
 
+f = open("end_to_end.txt", "w")
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
@@ -18,11 +19,14 @@ for i in range(1000):
   sendtime = datetime.datetime.strptime(raw_message, "%Y-%m-%d %H:%M:%S.%f")
   delay = (datetime.datetime.now()-sendtime).microseconds/1000.0
   totaltime += delay
-  print(str(delay))
+  f.write(str(delay) + "\n")
+  #print(str(delay))
   clientMsg = "Message from Client:{}".format(message)
   clientIP  = "Client IP Address:{}".format(address)
   #print(clientMsg)
   #print(clientIP)
 
   UDPServerSocket.sendto(message, address)
-print("total: " + str(totaltime/1000.0))
+print("avg: " + str(totaltime/1000.0))
+f.write("avg: " + str(totaltime/1000.0))
+f.close()
