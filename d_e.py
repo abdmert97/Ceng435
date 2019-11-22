f55a6732803e44740e618f06daa9d82bfd700356
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 19:31:09 +0300

    Update d_e.py

--- d_e.py
+++ d_e.py
@@ -7,19 +7,19 @@ bufferSize  = 1024
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
+for i in range(1000)
+  bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
 
-bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+  message = bytesAddressPair[0]
+  address = bytesAddressPair[1]
 
-message = bytesAddressPair[0]
-address = bytesAddressPair[1]
+  raw_message = "{}".format(message)
+  sendtime = datetime.datetime.strptime(raw_message, "%Y-%m-%d %H:%M:%S.%f")
+  delay = datetime.datetime.now().microsecond-sendtime.microsecond
+  print("formatted: " + str(delay/1000.0))
+  clientMsg = "Message from Client:{}".format(message)
+  clientIP  = "Client IP Address:{}".format(address)
+  print(clientMsg)
+  print(clientIP)
 
-raw_message = "{}".format(message)
-sendtime = datetime.datetime.strptime(raw_message, "%Y-%m-%d %H:%M:%S.%f")
-delay = datetime.datetime.now().microsecond-sendtime.microsecond
-print("formatted: " + str(delay/1000.0))
-clientMsg = "Message from Client:{}".format(message)
-clientIP  = "Client IP Address:{}".format(address)
-print(clientMsg)
-print(clientIP)
-
-UDPServerSocket.sendto(message, address)
+  UDPServerSocket.sendto(message, address)
