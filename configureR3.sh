68fbd2790d2ed6dce255fdb0c18f8a5a2c5e7d6b
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 20:42:43 +0300

    Update configureR3.sh

--- configureR3.sh
+++ configureR3.sh
@@ -6,5 +6,5 @@ s_adapter=$(ip route get $s | grep -Po '(?<=(dev )).*(?= src| proto)')
 d_adapter=$(ip route get $d | grep -Po '(?<=(dev )).*(?= src| proto)')
 
 
-sudo tc qdisc add dev $s_adapter root netem delay ${1}ms ${2}ms distribution normal
-sudo tc qdisc add dev $d_adapter root netem delay ${1}ms ${2}ms distribution normal
+sudo tc qdisc add dev $s_adapter root netem delay "$1"ms "$2"ms distribution normal
+sudo tc qdisc add dev $d_adapter root netem delay "$1"ms "$2"ms distribution normal
