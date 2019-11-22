205bb7cf10c7720c04b4a52895d255e6b821f7f6
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 17:33:08 +0300

    Update d_f.py

--- d_f.py
+++ d_f.py
@@ -1,12 +1,15 @@
 import socket
 from threading import Thread 
 
+# SETTINGS
 localIP     ="10.10.5.2"
 localPorts   = [30410, 30420, 30430]
+
+msgCount = 1000
 bufferSize  = 1024
+msgFromClient = "testtesttest"
 
-msgFromClient = "Hello UDP Server"
-
+# Init
 bytesToSend = str.encode(msgFromClient)
 
 
@@ -14,19 +17,20 @@ bytesToSend = str.encode(msgFromClient)
 def server(i):  
     UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     UDPServerSocket.bind((localIP, localPorts[i]))
-    for x in range(1000):
-        #print("UDP thread"+str(i)+" up and listening")
+    for x in range(msgCount):
+        print("UDP thread"+str(i)+" up and listening")
         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
 
         message = bytesAddressPair[0]
         address = bytesAddressPair[1]
         clientMsg = "Message from Client:{}".format(message)
         clientIP  = "Client IP Address:{}".format(address)
-        #print(clientMsg)
-        #print(clientIP)
+        print(clientMsg)
+        print(clientIP)
 
         bytesToSend         = str.encode(str(message).upper())
         UDPServerSocket.sendto(bytesToSend, address)
     UDPServerSocket.close()
+    
 servers = [Thread(target=server, args=(i,)) for i in range(3)]
 for sv in servers: sv.start()
