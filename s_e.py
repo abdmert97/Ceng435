7e1596424e37c07c747005f22aac0f7e49cf46fa
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 18:41:20 +0300

    Update s_e.py

--- s_e.py
+++ s_e.py
@@ -1,18 +1,16 @@
 import socket
 import datetime
 
-msgFromClient       = "testtesttest"
-
-bytesToSend         = str.encode(msgFromClient)
-
 r3Address = ("10.10.6.2", 30300)
 
 bufferSize          = 1024
 
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-#UDPClientSocket.connect((serverAddressPort))
-# Send to server using created UDP socket
+
+a = datetime.datetime.now()
+msgFromClient = str(a)
+bytesToSend         = str.encode(msgFromClient)
 UDPClientSocket.sendto(bytesToSend, r3Address)
 
 msgFromServer = UDPClientSocket.recvfrom(bufferSize)
