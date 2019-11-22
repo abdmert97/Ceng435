29022ac15472da047069ee2b5a282a4a73e646d3
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 18:04:08 +0300

    Update r1_f.py

--- r1_f.py
+++ r1_f.py
@@ -8,9 +8,6 @@ localIP     ="10.10.4.1"
 localPorts   = [30120]
 bufferSize  = 1024
 
-clientCount = 2
-serverCount = 1
-
 msgCount = 1000
 msgFromClient = "testtesttest"
 
@@ -20,8 +17,8 @@ dAddress = ("10.10.5.2", 30410)
 # Init
 bytesToSend = str.encode(msgFromClient)
 serverAddressPorts = [sAddress, dAddress]
-testResults = []
-testAverages = []
+f = open("link_costs.txt", "w")
+
 
 def server(i):  
     UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -42,11 +39,11 @@ def server(i):
     UDPServerSocket.close()
 
 
-def client(i):
+def client(i): 
+    f.write("--- Individual Tests ---\n")
     totaltime = 0
     # Create a UDP socket at client side
     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    turnResults = []
     for x in range(msgCount):
         #UDPClientSocket.connect((serverAddressPorts[i]))
         # Send to server using created UDP socket
@@ -60,23 +57,16 @@ def client(i):
         #print(c.microseconds/1000.0)
         msg = "Message from Server {}".format(msgFromServer[0])
         #print(msg)
-        turnResults.append((c.microseconds)/1000.0)
-    testResults.append(turnResults)
+        f.write(str(x) + " - " + str((c.microseconds)/1000.0) + "\n")
     UDPClientSocket.close()
-    testAverages[i] = (totaltime/msgCount)/1000.0
+    f.write("--- Average Cost ---\n")
+    f.write(str(i) + " - " + str((totaltime/msgCount)/1000.0) + "\n")
     print(str((totaltime/msgCount)/1000.0) + "avg for " + str(i)) 
 
-clients = [Thread(target=client, args=(i,)) for i in range(clientCount)]
+clients = [Thread(target=client, args=(i,)) for i in range(2)]
 for cl in clients: cl.start()
-servers = [Thread(target=server, args=(i,)) for i in range(serverCount)]
+servers = [Thread(target=server, args=(i,)) for i in range(1)]
 for sv in servers: sv.start()
 for sv in servers: sv.join()
 for cl in clients: cl.join()
-f = open("link_costs.txt", "w")
-for i in range(clientCount):
-    f.write("--- Individual Tests for" + str(i) + "  ---\n")
-    for j in range(msgCount):
-        f.write(str(i) + "->" + str(j) + " - " + str(testResults[i][j]) + "\n")
-    f.write("--- Average Cost ---\n")
-    f.write(str(i) + " - " + str(testAverages[i]) + "\n")
 f.close()
