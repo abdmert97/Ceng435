d111cad744c1006b546c32ad03cc0ec3db0bafab
Ramazan Selim Şahin <47250947+rselimsahin@users.noreply.github.com>
2019-11-22 19:43:54 +0300

    Update d_e.py

--- d_e.py
+++ d_e.py
@@ -7,6 +7,7 @@ bufferSize  = 1024
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
+totaltime = 0
 for i in range(1000):
   bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
 
@@ -16,6 +17,7 @@ for i in range(1000):
   raw_message = "{}".format(message)
   sendtime = datetime.datetime.strptime(raw_message, "%Y-%m-%d %H:%M:%S.%f")
   delay = datetime.datetime.now().microsecond-sendtime.microsecond
+  totaltime += delay
   print("formatted: " + str(delay/1000.0))
   clientMsg = "Message from Client:{}".format(message)
   clientIP  = "Client IP Address:{}".format(address)
@@ -23,3 +25,4 @@ for i in range(1000):
   print(clientIP)
 
   UDPServerSocket.sendto(message, address)
+print(totaltime/1000.0)
